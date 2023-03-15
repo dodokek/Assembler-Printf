fd440433362df0d401ad3778ce3eb65296a98440
Aleksander Morozov <alex.morozov.dd@mail.ru>
2023-03-15 21:46:14 +0300

    С божьей помощью вывел строчку

--- printf.s
+++ printf.s
@@ -1,17 +1,72 @@
+section .data
+
+buffer: times 64 db 12
+
+template_str: db "Ya pidoras", 0
+
+;------------------------------------------------
+
 section .text
 
 global _start
 
-
-;--------------------------------
-
 _start:
+
+	mov rsi, template_str
+	call PrintfMain
 
     mov eax,0x1          ; exiting the application like cool progers
 	mov ebx,0            ; err code = 0
 	int 80h              ; calling interrupt
 
 
-section .data
+;------------------------------------------------
+; Prints arguments in the cmd according to 
+; template string
+;------------------------------------------------
+;  Entry: rsi - pointer to template string
+;  		  r1  - first arg
+;		  r2  - second arg
+;
+;  Destroys:
+;
+;  Expects: template strings ends on \0
+;------------------------------------------------
 
-db	
+PrintfMain:
+
+.loop:
+	mov al, [rsi]	; al = *tmpl_string
+
+	cmp al, 0	; if al == \0 then return
+	je .end
+
+	cmp al, '%'		; if al == % then handle argument
+	; je HandleArg
+
+	call Putch		; print char
+
+
+	inc rsi			; ptr++
+	jmp .loop
+
+.end:
+	ret
+
+
+Putch:
+
+	push rcx 	; i still havent figure out, but syscall destroys rcx and r11, so restore them
+	push r11
+
+	mov rdx, 1   ; msg len
+	mov rdi, 1	 ; file descriptor
+	mov rax, 1   ; syscall for write()
+;   mov rsi, rsi ; no need cuz in prev func it is already set
+
+	syscall
+
+	pop r11
+	pop rcx
+
+	ret
