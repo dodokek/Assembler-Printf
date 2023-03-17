a98055e77057b9977da512ff02e214284b4d67b0
Aleksander Morozov <alex.morozov.dd@mail.ru>
2023-03-17 20:35:42 +0300

    sheeeeeeeeeeeeesh

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ main : printf.o run_print.o
 printf.o : printf.s 
 	nasm -f elf64 -g printf.s -o printf.o
 
-run_print.o : run_print.cpp
-	gcc	-c run_print.cpp -o run_print.o
+run_print.o : run_print.c
+	gcc	-c run_print.c -o run_print.o
 
 clear:
 	rm *.o
--- run_print.c
+++ run_print.c
@@ -3,6 +3,9 @@ extern void DodoPrint (const char* template_string, ...);
 int main()
 {
     DodoPrint ("Bebrochka!\n");
+
+    DodoPrint ("У меня %d хромосом.\n", 47);
     
+    DodoPrint ("Hi %% %d %x %s %b \n", 127, 23, "Kekich", 65);
     return 0;
 }
